7b695a9e85644a2e610c38d3ffbf023b4365c238
Borys Szefczyk <boryszef@gmail.com>
2018-12-05 22:03:44 +0100

    pretty looks

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -5,7 +5,13 @@
         .controller('NarrowItDownController', NarrowItDownController)
         .service('MenuSearchService', MenuSearchService)
         .directive('foundItems', FoundItemsDirective)
-        .value('menuUrl', "https://davids-restaurant.herokuapp.com/menu_items.json");
+        .value('config', {
+            menuUrl: 'https://davids-restaurant.herokuapp.com/menu_items.json',
+            searchButtonText: 'Narrow It Down For Me!',
+            progressText: 'In progress',
+            notFoundText: 'Nothing found',
+            rejectButtonText: 'Don\'t want this one!'
+        });
 
 
     function FoundItemsDirective () {
@@ -13,6 +19,7 @@
             scope: {
                 items: '<',
                 message: '<',
+                buttonText: '<',
                 onRemove: '&'
             },
             templateUrl: 'foundItems.html',
@@ -25,23 +32,30 @@
     }
 
 
-    NarrowItDownController.$inject = ['MenuSearchService'];
-    function NarrowItDownController (MenuSearchService) {
+    NarrowItDownController.$inject = ['MenuSearchService', 'config'];
+    function NarrowItDownController (MenuSearchService, config) {
         var menu = this;
+        menu.spinner = false;
+        menu.buttonText = config.searchButtonText;
+        menu.rejectText = config.rejectButtonText;
         menu.searchTerm = '';
         menu.message = '';
         menu.found = [];
 
         menu.updateMessage = function () {
-            menu.message = menu.found.length ? '' : 'Nothing found';
+            menu.message = menu.found.length ? '' : config.notFoundText;
         };
 
         menu.narrowDown = function () {
+            menu.spinner = true;
+            menu.buttonText = config.progressText;
             var promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
 
             promise.then(function (response) {
                 menu.found = response;
                 menu.updateMessage();
+                menu.spinner = false;
+                menu.buttonText = config.searchButtonText;
             });
         };
 
@@ -54,8 +68,8 @@
     }
 
 
-    MenuSearchService.$inject = ['$q', '$http', 'menuUrl'];
-    function MenuSearchService ($q, $http, menuUrl) {
+    MenuSearchService.$inject = ['$q', '$http', 'config'];
+    function MenuSearchService ($q, $http, config) {
         var service = this;
 
         service.getMatchedMenuItems = function (searchTerm) {
@@ -64,7 +78,7 @@
                     resolve([]);
                 });
 
-            let promise = $http.get(menuUrl);
+            let promise = $http.get(config.menuUrl);
 
             function searchInDescription(item) {
                 return item['description'].toLowerCase().includes(searchTerm);
